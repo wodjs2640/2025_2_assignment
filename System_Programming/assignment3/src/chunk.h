9a3bf2346cf9e1a98989649a1695b5305d87e776
wodjs2640 <47349711+wodjs2640@users.noreply.github.com>
2025-10-23 20:16:23 +0900

    작성완료

---
+++ System_Programming/assignment3/src/chunk.h
@@ -0,0 +1,12 @@
+/*--------------------------------------------------------------------*/
+/* This file is almost empty, and is provided to enable you to use    */ 
+/* the Makefile.                                                      */
+/* You are free to modify this file if you want.                      */
+/* Even if you do not use this file, please keep it for the Makefile  */
+/* and be sure to include it when you submit your assignment.         */
+/*--------------------------------------------------------------------*/
+
+#ifndef _CHUNK_STUB_H_
+#define _CHUNK_STUB_H_
+
+#endif
